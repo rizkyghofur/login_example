9165cf470df88a8b79d9b0bbca651f3af82b4f29
Rizki Abdul Gofur <49636147+rizkyghofur@users.noreply.github.com>
2022-04-07 10:42:26 +0700

    Update URLConstants.dart

--- lib/constants/URLConstants.dart
+++ lib/constants/URLConstants.dart
@@ -1,14 +1,12 @@
 class UrlConstants {
-  static const login = "https://fa52-61-5-57-88.ngrok.io/api/login-customer";
+  static const login = "https://e178-61-5-57-88.ngrok.io/api/login-customer";
   static const register =
-      "https://fa52-61-5-57-88.ngrok.io/api/daftar-customer";
-  static const get_data = "https://fa52-61-5-57-88.ngrok.io/api/list-produk";
-  static const getImagelink =
-      "http://testing.rizkyghofur.my.id/public/image/admin/menu/normal/";
-  static const add_data = "https://fa52-61-5-57-88.ngrok.io/api/create-produk/";
+      "https://e178-61-5-57-88.ngrok.io/api/daftar-customer";
+  static const get_data = "https://e178-61-5-57-88.ngrok.io/api/list-produk";
+  static const add_data = "https://e178-61-5-57-88.ngrok.io/api/create-produk/";
   static const update_data =
-      "https://fa52-61-5-57-88.ngrok.io/api/update-produk/";
+      "https://e178-61-5-57-88.ngrok.io/api/update-produk/";
   static const delete_data =
-      "https://fa52-61-5-57-88.ngrok.io/api/delete-produk/";
-  static const getBiodata = "https://fa52-61-5-57-88.ngrok.io/api/getbiodata";
+      "https://e178-61-5-57-88.ngrok.io/api/delete-produk/";
+  static const getBiodata = "https://e178-61-5-57-88.ngrok.io/api/getbiodata";
 }
