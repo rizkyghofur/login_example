d3afc6f92c4f1da7a23e0f91bf722cb38199c744
Rizki Abdul Gofur <49636147+rizkyghofur@users.noreply.github.com>
2022-04-08 10:28:58 +0700

    Update URLConstants.dart

--- lib/constants/URLConstants.dart
+++ lib/constants/URLConstants.dart
@@ -1,13 +1,12 @@
 class UrlConstants {
-  static const login = "https://e178-61-5-57-88.ngrok.io/api/login";
-  static const register = "https://e178-61-5-57-88.ngrok.io/api/register";
-  static const get_data = "https://e178-61-5-57-88.ngrok.io/api/list-produk";
-  static const get_data_by_id =
-      "https://e178-61-5-57-88.ngrok.io/api/show-produk/";
-  static const add_data = "https://e178-61-5-57-88.ngrok.io/api/create-produk/";
-  static const update_data =
-      "https://e178-61-5-57-88.ngrok.io/api/update-produk/";
-  static const delete_data =
-      "https://e178-61-5-57-88.ngrok.io/api/delete-produk/";
-  static const getBiodata = "https://e178-61-5-57-88.ngrok.io/api/getbiodata";
+  String endpoint = "https://e178-61-5-57-88.ngrok.io/";
+  
+  static const login = endpoint + "api/login";
+  static const register = endpoint + "api/register";
+  static const get_data = endpoint + "api/list-produk";
+  static const get_data_by_id = endpoint + "api/show-produk/";
+  static const add_data = endpoint + "api/create-produk/";
+  static const update_data = endpoint + "api/update-produk/";
+  static const delete_data = endpoint + "api/delete-produk/";
+  static const getBiodata = endpoint + "api/getbiodata";
 }
